11ee0bd0c33cdf599c4ecd1f35bf2606a80f9e69
cynthianyeint <nyeintnyeintkhin.92@gmail.com>
2016-10-05 11:20:48 +0800

    test cynthia

--- SA43Team05B/Model/LMSModel.Designer.cs
+++ SA43Team05B/Model/LMSModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Vasanth Kumar P\Documents\NUS\ER.NET\CA\SA43Team05B\SA43Team05B\Model\LMSModel.edmx'. 
+﻿// T4 code generation is enabled for model 'Z:\LibraryManagementSystem\SA43Team05B\Model\LMSModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
