8a8b2283be3739656ed4cc5e424bb95665b24fd7
WangXinwei <WangXinwei@DESKTOP-6VHU3KN>
2016-10-05 13:34:54 +0800

    Add BookDetailsForm

--- SA43Team05B/View/BookDetailsForm.Designer.cs
+++ SA43Team05B/View/BookDetailsForm.Designer.cs
@@ -28,50 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.LblID = new System.Windows.Forms.Label();
+            this.LblBookTitle = new System.Windows.Forms.Label();
+            this.TxtbBookTitle = new System.Windows.Forms.TextBox();
+            this.TxtbBookID = new System.Windows.Forms.TextBox();
             this.BookDetailsTitleLbl = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.LblBookID = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // LblID
+            // LblBookTitle
             // 
-            this.LblID.AutoSize = true;
-            this.LblID.Location = new System.Drawing.Point(105, 77);
-            this.LblID.Name = "LblID";
-            this.LblID.Size = new System.Drawing.Size(31, 25);
-            this.LblID.TabIndex = 0;
-            this.LblID.Text = "ID";
+            this.LblBookTitle.AutoSize = true;
+            this.LblBookTitle.Location = new System.Drawing.Point(424, 235);
+            this.LblBookTitle.Name = "LblBookTitle";
+            this.LblBookTitle.Size = new System.Drawing.Size(38, 20);
+            this.LblBookTitle.TabIndex = 8;
+            this.LblBookTitle.Text = "Title";
+            // 
+            // TxtbBookTitle
+            // 
+            this.TxtbBookTitle.Location = new System.Drawing.Point(479, 235);
+            this.TxtbBookTitle.Name = "TxtbBookTitle";
+            this.TxtbBookTitle.Size = new System.Drawing.Size(104, 26);
+            this.TxtbBookTitle.TabIndex = 6;
+            // 
+            // TxtbBookID
+            // 
+            this.TxtbBookID.Location = new System.Drawing.Point(479, 190);
+            this.TxtbBookID.Name = "TxtbBookID";
+            this.TxtbBookID.Size = new System.Drawing.Size(104, 26);
+            this.TxtbBookID.TabIndex = 7;
             // 
             // BookDetailsTitleLbl
             // 
             this.BookDetailsTitleLbl.AutoSize = true;
             this.BookDetailsTitleLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BookDetailsTitleLbl.Location = new System.Drawing.Point(36, 26);
+            this.BookDetailsTitleLbl.Location = new System.Drawing.Point(24, 55);
             this.BookDetailsTitleLbl.Name = "BookDetailsTitleLbl";
             this.BookDetailsTitleLbl.Size = new System.Drawing.Size(143, 29);
-            this.BookDetailsTitleLbl.TabIndex = 1;
+            this.BookDetailsTitleLbl.TabIndex = 5;
             this.BookDetailsTitleLbl.Text = "BookDetails";
             // 
-            // textBox1
+            // LblBookID
             // 
-            this.textBox1.Location = new System.Drawing.Point(160, 77);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(104, 30);
-            this.textBox1.TabIndex = 2;
+            this.LblBookID.AutoSize = true;
+            this.LblBookID.Location = new System.Drawing.Point(424, 190);
+            this.LblBookID.Name = "LblBookID";
+            this.LblBookID.Size = new System.Drawing.Size(26, 20);
+            this.LblBookID.TabIndex = 4;
+            this.LblBookID.Text = "ID";
             // 
             // BookDetailsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(557, 381);
-            this.Controls.Add(this.textBox1);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.ClientSize = new System.Drawing.Size(1066, 576);
+            this.Controls.Add(this.LblBookTitle);
+            this.Controls.Add(this.TxtbBookTitle);
+            this.Controls.Add(this.TxtbBookID);
             this.Controls.Add(this.BookDetailsTitleLbl);
-            this.Controls.Add(this.LblID);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Controls.Add(this.LblBookID);
             this.Name = "BookDetailsForm";
-            this.Text = "LibrarySystemManage";
-            this.Load += new System.EventHandler(this.BookDetailsForm_Load);
+            this.Controls.SetChildIndex(this.LblBookID, 0);
+            this.Controls.SetChildIndex(this.BookDetailsTitleLbl, 0);
+            this.Controls.SetChildIndex(this.TxtbBookID, 0);
+            this.Controls.SetChildIndex(this.TxtbBookTitle, 0);
+            this.Controls.SetChildIndex(this.LblBookTitle, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,8 +99,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Label LblID;
+        private System.Windows.Forms.Label LblBookTitle;
+        private System.Windows.Forms.TextBox TxtbBookTitle;
+        private System.Windows.Forms.TextBox TxtbBookID;
         private System.Windows.Forms.Label BookDetailsTitleLbl;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label LblBookID;
     }
 }
--- SA43Team05B/View/BookDetailsForm.cs
+++ SA43Team05B/View/BookDetailsForm.cs
@@ -3,29 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SA43Team05B.View
 {
-    public partial class BookDetailsForm : Form
+    public partial class BookDetailsForm : SA43Team05B.MainMenu
     {
-        
         public BookDetailsForm()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void BookDetailsForm_Load(object sender, EventArgs e)
-        {
-            
         }
     }
 }
